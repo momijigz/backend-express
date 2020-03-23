72370fcd5de5cf64aa628a8ce870c95cd1e23a19
gmaijoe <gmai95h@gmail.com>
2020-03-23 02:30:39 -0700

    more

--- routes/index.js
+++ routes/index.js
@@ -506,7 +506,11 @@ router.get('/discover/:page', auth, async (req, res) => {
 
       switch (entry.type) {
         case 'Post':
-          let foundPost = await Post.findOne({ _id: entry.postId, completed: false, assignedUser: undefined });
+          let foundPost = await Post.findOne({
+            _id: entry.postId,
+            completed: false,
+            // assignedUser: undefined
+          });
           if (foundPost) {
             compiledNewsfeed.push(foundPost);
           }
